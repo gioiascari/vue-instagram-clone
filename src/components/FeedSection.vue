a14d7ba5187cda460bcb380b23fe8c518fb6296f
gioiascari <gioia.ascari96@gmail.com>
2022-08-08 20:02:23 +0200

    add img

--- src/components/FeedSection.vue
+++ src/components/FeedSection.vue
@@ -21,7 +21,7 @@
       <!-- /Icons -->
     </v-layout>
     <v-row class="mt-5">
-      <v-col>
+      <v-col v-for="(image, i) in feedImg" :key="i">
         <v-card
           elevation="12"
           height="230"
@@ -29,6 +29,7 @@
           color="blue"
           class="mr-4"
         >
+          <v-img height="230" :src="image"></v-img>
         </v-card>
       </v-col>
     </v-row>
@@ -36,7 +37,21 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "FeedSection",
+  data() {
+    return {
+      feedImg: [
+        "https://images.unsplash.com/photo-1502429892517-ebe1510442a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
+        "https://images.unsplash.com/photo-1584701782235-beff0661e16a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+        "https://images.unsplash.com/photo-1492056128351-09fb7a99969e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=562&q=80",
+        "https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1515966097209-ec48f3216288?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
