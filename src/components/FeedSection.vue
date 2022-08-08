764ae22034e592f02146498d3f4bd14464b62bb6
gioiascari <gioia.ascari96@gmail.com>
2022-08-08 19:44:39 +0200

    init Feed Section

---
+++ src/components/FeedSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-10">
+    <v-layout>
+      <v-flex>
+        <div class="title">Latest Feed</div>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
