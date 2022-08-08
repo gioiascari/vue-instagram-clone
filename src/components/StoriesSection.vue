30fb3713b6a80062ccb8d6c950685051b6a6cbed
gioiascari <gioia.ascari96@gmail.com>
2022-08-08 19:42:41 +0200

    Stories Section

--- src/components/StoriesSection.vue
+++ src/components/StoriesSection.vue
@@ -4,6 +4,45 @@
       <v-flex>
         <div class="title">Featured Stories</div>
       </v-flex>
+      <v-flex text-right>
+        <v-btn depressed class="text-none" color="grey lighten-4">
+          <span class="grey--text"> Popular Stories </span>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout justify-start class="mt-7">
+      <!-- Image 1 -->
+      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
+        <v-img
+          height="230"
+          src="https://images.unsplash.com/photo-1550792220-553fd02a239c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80"
+        ></v-img>
+      </v-card>
+      <!-- /Image 1 -->
+      <!-- Image 2 -->
+      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
+        <v-img
+          height="230"
+          src="https://images.unsplash.com/photo-1659935275716-69e12c6499e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+        ></v-img>
+      </v-card>
+      <!-- /Image 2 -->
+      <!-- Image 3 -->
+      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
+        <v-img
+          height="230"
+          src="https://images.unsplash.com/photo-1561832469-637830b89468?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80"
+        ></v-img>
+      </v-card>
+      <!-- /Image 3 -->
+      <!-- Image 4 -->
+      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
+        <v-img
+          height="230"
+          src="https://images.unsplash.com/photo-1659879003706-378e6b74d461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+        ></v-img>
+      </v-card>
+      <!-- /Image 4 -->
     </v-layout>
   </div>
 </template>
