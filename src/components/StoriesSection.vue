9d6972a48e91443f72e1592c0f36582033bd346f
gioiascari <gioia.ascari96@gmail.com>
2022-08-08 22:01:46 +0200

    add data for stories account

--- src/components/StoriesSection.vue
+++ src/components/StoriesSection.vue
@@ -10,45 +10,65 @@
         </v-btn>
       </v-flex>
     </v-layout>
-    <v-layout justify-start class="mt-7">
+    <v-layout justify-start text-center class="mt-7">
       <!-- Image 1 -->
-      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
-        <v-img
-          height="230"
-          src="https://images.unsplash.com/photo-1550792220-553fd02a239c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80"
-        ></v-img>
-      </v-card>
+      <v-flex v-for="(account, i) in storiesAccount" :key="i">
+        <v-avatar class="outlined" size="80">
+          <v-avatar width="100%" height="100">
+            <img :src="account.img" size="80" />
+          </v-avatar>
+        </v-avatar>
+        <div class="subtitle-2">{{ account.name }}</div>
+        <!--Card-->
+      </v-flex>
+
       <!-- /Image 1 -->
-      <!-- Image 2 -->
-      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
-        <v-img
-          height="230"
-          src="https://images.unsplash.com/photo-1659935275716-69e12c6499e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
-        ></v-img>
-      </v-card>
-      <!-- /Image 2 -->
-      <!-- Image 3 -->
-      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
-        <v-img
-          height="230"
-          src="https://images.unsplash.com/photo-1561832469-637830b89468?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80"
-        ></v-img>
-      </v-card>
-      <!-- /Image 3 -->
-      <!-- Image 4 -->
-      <v-card elevation="12" height="230" width="170" color="blue" class="mr-4">
-        <v-img
-          height="230"
-          src="https://images.unsplash.com/photo-1659879003706-378e6b74d461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-        ></v-img>
-      </v-card>
-      <!-- /Image 4 -->
     </v-layout>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "StoriesSection",
+  data() {
+    return {
+      storiesAccount: [
+        {
+          name: "pablo",
+          img: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        },
+        {
+          name: "ghost.u",
+          img: "https://images.unsplash.com/photo-1507038732509-8b1a9623223a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        },
+        {
+          name: "aioig",
+          img: "https://images.unsplash.com/photo-1571914001156-dca4c3a20844?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+        },
+        {
+          name: "iracsa",
+          img: "https://images.unsplash.com/photo-1508002366005-75a695ee2d17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=368&q=80",
+        },
+        {
+          name: "badaboom",
+          img: "https://images.unsplash.com/photo-1519689373023-dd07c7988603?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        },
+        {
+          name: "default-name",
+          img: "https://images.unsplash.com/photo-1558920558-feb6e762d597?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        },
+        {
+          name: "bye-bye",
+          img: "https://images.unsplash.com/photo-1613467678197-6054e47c5fc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+        },
+        {
+          name: "pippo",
+          img: "https://images.unsplash.com/photo-1615268485840-1616aebe9898?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=383&q=80",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
