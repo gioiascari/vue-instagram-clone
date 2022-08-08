010b5d88d7ecf705aec11849b2a3ca7285866b6f
gioiascari <gioia.ascari96@gmail.com>
2022-08-08 22:21:13 +0200

    add some style

--- src/components/FeedSection.vue
+++ src/components/FeedSection.vue
@@ -20,16 +20,15 @@
       </v-flex>
       <!-- /Icons -->
     </v-layout>
-    <v-row class="mt-5">
+
+    <v-row class="mt-5" text-left>
       <v-col v-for="(image, i) in feedImg" :key="i">
-        <v-card
-          elevation="12"
-          height="230"
-          width="210"
-          color="blue"
-          class="mr-4"
-        >
-          <v-img height="230" :src="image"></v-img>
+        <v-card height="230" width="210" class="mr-4 my-5" outlined>
+          <div class="subtitle-2 pa-md-4">
+            {{ image.name }}
+          </div>
+          <v-img height="230" :src="image.img"></v-img>
+          <div class="subtitle-2 pa-md-4">ciao</div>
         </v-card>
       </v-col>
     </v-row>
@@ -42,12 +41,30 @@ export default {
   data() {
     return {
       feedImg: [
-        "https://images.unsplash.com/photo-1502429892517-ebe1510442a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        "https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
-        "https://images.unsplash.com/photo-1584701782235-beff0661e16a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
-        "https://images.unsplash.com/photo-1492056128351-09fb7a99969e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=562&q=80",
-        "https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        "https://images.unsplash.com/photo-1515966097209-ec48f3216288?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        {
+          name: "uiuxdesign.trends",
+          img: "https://images.unsplash.com/photo-1502429892517-ebe1510442a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        },
+        {
+          name: "mattwmurray",
+          img: "https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
+        },
+        {
+          name: "lauvdahl",
+          img: "https://images.unsplash.com/photo-1584701782235-beff0661e16a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+        },
+        {
+          name: "southpark",
+          img: "https://images.unsplash.com/photo-1492056128351-09fb7a99969e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=562&q=80",
+        },
+        {
+          name: "beyonce",
+          img: "https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        },
+        {
+          name: "wacom",
+          img: "https://images.unsplash.com/photo-1515966097209-ec48f3216288?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        },
       ],
     };
   },
